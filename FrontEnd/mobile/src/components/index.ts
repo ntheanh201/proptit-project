f7f368386c46dcd7e4a3ace4ce13263ba9e1c27f
Rambo <hai.nguyentien.029@gmail.com>
2020-01-17 17:31:17 +0700

    Delete all Comps & Screens

--- FrontEnd/mobile/src/components/index.ts
+++
@@ -1,7 +0,0 @@
-import ButtonText from './ButtonText'
-import EditText from './EditText'
-import LoadingView from './LoadingView'
-import MenuHomeSideBar from './MenuHomeSideBar'
-import ButtonWithIcon from './ButtonWithIcon'
-
-export { ButtonText, EditText, LoadingView, MenuHomeSideBar, ButtonWithIcon }
