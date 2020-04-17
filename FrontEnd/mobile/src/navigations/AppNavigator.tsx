47475b8b2f869a0ed185a9b65c9a4dd51c7fc990
Tran Duy Cong Khanh <khanhcongpruj123@gmail.com>
2020-04-17 17:36:46 +0700

    update gitignore (Podlife.lock)

--- FrontEnd/mobile/src/navigations/AppNavigator.tsx
+++ FrontEnd/mobile/src/navigations/AppNavigator.tsx
@@ -49,32 +49,4 @@ const AppNavigator = () => {
   )
 }
 
-// const AppNavigator = createSwitchNavigator(
-//   {
-//     SplashStack: {
-//       screen: SplashScreen,
-//       navigationOptions: {
-//         header: null,
-//       },
-//     },
-//     AuthStack: createStackNavigator({
-//       SignIn: {
-//         screen: SignInScreen,
-//         navigationOptions: {
-//           header: null,
-//         },
-//       },
-//     }),
-//     HomeStack: {
-//       screen: HomeNavigator,
-//       navigationOptions: {
-//         header: null,
-//       },
-//     },
-//   },
-//   {
-//     initialRouteName: 'SplashStack',
-//   },
-// )
-
 export default AppNavigator
