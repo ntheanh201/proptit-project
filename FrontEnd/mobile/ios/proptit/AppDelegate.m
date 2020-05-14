48e276b12190e4a76afb1968868c4f664ec3da07
Rambo <hai.nguyentien.0209@gmail.com>
2020-05-14 21:10:03 +0700

    Fix run on iOS

--- FrontEnd/mobile/ios/proptit/AppDelegate.m
+++ FrontEnd/mobile/ios/proptit/AppDelegate.m
@@ -46,7 +46,7 @@ static void InitializeFlipper(UIApplication *application) {
   rootViewController.view = rootView;
   self.window.rootViewController = rootViewController;
   [self.window makeKeyAndVisible];
-  [RNSplashScreen show];
+//  [RNSplashScreen show];
   return YES;
 }
 
